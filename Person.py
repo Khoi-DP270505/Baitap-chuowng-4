56736b4919aa03771651905a199479d8ecfac285
Khoi-DP270505 <23011839@st.phenikaa-uni.edu.vn>
2024-09-16 14:24:49 +0700

    Update Person.py

--- Person.py
+++ Person.py
@@ -1,4 +1,4 @@
-# person.py và child.py gộp chung
+
 
 class Person:
     def __init__(self, name, age):
@@ -9,10 +9,10 @@ class Child(Person):
     def __init__(self, name, age):
         super().__init__(name, age)
 
-# test code
+
 person = Person("Peter", 25)
 child = Child("Peter Junior", 5)
 
-print(person.name)  # Output: Peter
-print(person.age)   # Output: 25
-print(child.__class__.__bases__[0].__name__)  # Output: Person
+print(person.name) 
+print(person.age)   
+print(child.__class__.__bases__[0].__name__)  
